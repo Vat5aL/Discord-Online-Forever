4ee7d0c6785d7ca555f5ee5351d40372622ef1b6
VatsaL <78130088+Vat5aL@users.noreply.github.com>
2024-03-08 18:26:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Eris = require("eris");
 const keep_alive = require('./keep_alive.js')
 
 // Replace TOKEN with your bot account's token
-const bot = new Eris(process.env.token1);
+const bot = new Eris(process.env.token);
 
 bot.on("error", (err) => {
   console.error(err); // or your preferred logger
